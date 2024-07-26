86559b4b9f2a99683221195dd91795cd501f44b2
truongngv2003 <truongnguyenmailbka@gmail.com>
2024-07-26 09:06:17 +0700

    add .env to save DB_URI

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-const MONGO_URI='mongodb+srv://freeCodeCampTruong:123CodeGa@cluster0.mgj6dtm.mongodb.net/QLSV?retryWrites=true&w=majority&appName=Cluster0'
+const MONGO_URI = process.env.MONGO_URI;
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGO_URI);
